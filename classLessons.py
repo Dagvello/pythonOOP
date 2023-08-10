38736ff834c539fcd2e1b76728d5fd5bdb9b6c8d
Dagvello <dagvello@ukr.net>
2023-08-10 17:52:25 +0300

    init

---
+++ classLessons.py
@@ -0,0 +1,21 @@
+class Dog():
+    """Создание класса Dog"""
+
+    def dog_say(self):
+        """Создание метода dog_say"""
+        print('Gav-Gav')
+
+
+class Cat():
+    """Создание класса Cat"""
+
+    def cat_say(self):
+        """Создание метода cat_say"""
+        print('Myu-Myu')
+
+
+Sharik = Dog()
+Tom = Cat()
+
+Sharik.dog_say()
+Tom.cat_say()
